9d5a226216484ead65cb34bfd0d14c3ea82231ea
Krish743 <142522152+Krish743@users.noreply.github.com>
2024-06-23 16:01:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div id="main">
+        <div id="nav">
+            <button id="button1">Menu</button>
+        </div>
+        <div id="top">
+            <div id="head">
+                <h1>Krish Patel<br>Digital Designer</h1>
+            </div>
+
+            <div id="head2">
+                <h2>(Gujarat,India )</h2>
+                <h2><i class="ri-arrow-right-down-line"></i>Scroll Down</h2>
+            </div>
+        </div>
+
+        <div id="ima"></div>
+
+
+        <div id="about1">
+            <h1>As a digital designer, I focus on producing top-notch and impactful digital experiences.</h1>
+            <div id="about2">
+                <h2>(About me)</h2>
+                <div id="icons">
+                   <a href="https://www.instagram.com/_krish_719/" target="_blank"> <i class="ri-instagram-line"></i></a>
+                    <a href="https://x.com/Krish9107" target="_blank"><i class="ri-twitter-fill"></i></a>
+                    <a href="https://open.spotify.com/user/ziy0f62t7anv3bbwmq6vvcing?si=926f1aae53314e5e" target="_blank"><i class="ri-spotify-fill"></i></a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
